740e3b0a8c81379ed62c5060e981fb289014c795
erikes <erikestrada37@outlook.com>
2024-05-06 15:22:01 -0600

    fix: :bug: environment access in firebaseConfig

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -1,6 +1,9 @@
+import * as dotenv from 'dotenv'
 import * as admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
+dotenv.config()
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
