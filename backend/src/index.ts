587e714cd213bb345f920cc93cf35da11e045716
erikes <erikestrada37@outlook.com>
2024-05-06 12:31:25 -0600

    feat: express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,16 @@
 import * as dotenv from 'dotenv'
+import express from 'express'
 dotenv.config()
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+app.get('/', (_req: any, res: any) => {
+    res.send('<h1>Tracking Server</h1>')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
