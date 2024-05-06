44928b6c36aa8dc6cf91d8468c5e1a8dce44c2b9
erikes <erikestrada37@outlook.com>
2024-05-06 12:38:43 -0600

    feat: vehicle api implementation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,8 @@
 import * as dotenv from 'dotenv'
 import express from 'express'
+
+import vehicleRouter from './routes/vehicle'
+
 dotenv.config()
 
 const app = express()
@@ -10,6 +13,8 @@ app.use(express.json())
 app.get('/', (_req: any, res: any) => {
     res.send('<h1>Tracking Server</h1>')
 })
+
+app.use('/api/v1/vehicle', vehicleRouter)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
