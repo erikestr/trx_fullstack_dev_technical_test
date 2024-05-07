b66c46c75560d808d38f840de2e0720df689a83a
erikestr <erikestrada37@outlook.com>
2024-05-06 23:51:04 -0600

    build: config for environment variables

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_GMAPS_API: string,
+    readonly VITE_GMAPS_MAPID: string
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv
+}
